7ac3a4808ee5fd93514d0449336b1549f660842a
mohanad <mohanad>
2022-05-27 10:24:00 -0700

    Initial commit

---
+++ task1.java
@@ -0,0 +1,56 @@
+public class task1 {
+
+    public static MyThreadVer3 createAndStart(String name){
+        MyThreadVer3 myThreadVer3 = new MyThreadVer3(name);
+        myThreadVer3.thread.start();
+        return myThreadVer3;
+    }
+
+    public void run(){
+        System.out.println(thread.getName() + "starts to operate");
+        try {
+            for (int count=0; count < 10; count++){
+                Thread.sleep(100);
+                System.out.println(thread.getName()+ " is executed, the counter value: "+count);
+            }
+
+        }catch (InterruptedException e){
+            System.out.println(thread.getName()+" has been interrupted");
+
+        }
+        System.out.println(thread.getName()+ " finishes running");
+
+    }
+
+}
+
+class Task1 {
+    public static void main(String[] args) {
+        System.out.println("The main thread starts running");
+
+        MyThreadVer3 myThread1 = MyThreadVer3.createAndStart("Child thread #1");
+        MyThreadVer3 myThread2 = MyThreadVer3.createAndStart("Child thread #2");
+        MyThreadVer3 myThread3= MyThreadVer3.createAndStart("Child thread #3");
+
+        /*do {
+            System.out.println(".");
+            try {
+                Thread.sleep(100);
+            }catch (InterruptedException e){
+                System.out.println("The main thread has been terminated");
+            }
+        }while (myThread1.thread.isAlive() || myThread2.thread.isAlive() || myThread3.thread.isAlive());
+            System.out.println("The main thread is exiting");*/
+
+
+        try
+        {   myThread1.thread.join();
+            myThread2.thread.join();
+            myThread3.thread.join();
+        }
+
+
+        catch(InterruptedException e)
+        {
+            System.out.println("The thread has terminated"+ e);
+}
